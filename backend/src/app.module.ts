926eb10c17f3dc72e73d6182a34e97cf92c8a425
anointedthedeveloper <anointedthedeveloper@gmail.com>
2026-03-28 18:18:38 +0100

    Initial Commit

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { CustomersModule } from './modules/customers/customers.module';
+import { DealsModule } from './modules/deals/deals.module';
+import { ActivitiesModule } from './modules/activities/activities.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    AuthModule,
+    CustomersModule,
+    DealsModule,
+    ActivitiesModule,
+  ],
 })
 export class AppModule {}
